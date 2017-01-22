fab1bdbe607a6b0bc83459f2ad0d815642527130
chih <chih.chen1029@gmail.com>
2017-01-22 18:18:27 +0000

    add validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
-    
+    validates :title, presence: true, length: {maximum: 50, minimum: 3}
+    validates :description, presence: true, length: {minimum: 10}
 end
+

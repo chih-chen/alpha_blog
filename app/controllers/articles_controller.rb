7ef997f8b6b448c3d586354422f5e5127f798bb1
chih <chih.chen1029@gmail.com>
2017-01-24 01:27:06 +0000

    edit and update working !

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,4 +17,23 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
+    def edit
+       @article = Article.find(params[:id]) 
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update({title: params[:article][:title], description: params[:article][:description]})
+            flash[:notice] = "Article successfuly updated!" 
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
+    end
+    
+    def index
+        
+    end
+        
 end

b453881f8227743617cb4e0098dd7f9275af0107
chih <chih.chen1029@gmail.com>
2017-01-25 22:26:41 +0000

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base #case sensitive, ignore the capitilize letter, so Joe is the same as joe
+   validates :username, 
+             presence: true, 
+             uniqueness: {case_sensitive: false},
+             length: {minimum:3, maximum:25}
    
+   #search for email regular expression, with the expression, u can verify if an email is valid ou not          
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
    
+   validades :email, 
+             presence:true,
+             uniqueness: {case_sensitive:false}, 
+             format: {with: VALID_EMAIL_REGEX}
 end

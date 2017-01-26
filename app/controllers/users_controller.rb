f4df975e55946ec4e93b1f3a6c5a2e4afff0cb27
chih <chih.chen1029@gmail.com>
2017-01-26 20:24:18 +0000

    user creation, edition, and partial fine!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,24 @@ class UsersController < ApplicationController
       end
    end
    
+   def edit 
+      @user = User.find(params[:id])
+   end
    
+   def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+         flash[:success] = "Account successfuly updated!"
+         redirect_to articles_path
+      else
+         #render the edit template again
+         render "edit"
+      end
+   end
+   
+   #this method gives me all attr from the params
+   #for safety, we use this method which will permit
+   # only these 3 params 
    private
    def user_params
       params.require(:user).permit(:username, :email, :password)

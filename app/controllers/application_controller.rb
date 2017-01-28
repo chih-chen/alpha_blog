955ad613b714cb2c0b4fd6c5639ea60964869a52
chih <chih.chen1029@gmail.com>
2017-01-28 04:17:45 +0000

    define session helper method in the application controller / implement some filter in the navbar when the user is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  #by including the module into the base class of all controllers (the Application controller), 
+  #we arrange to make them available in our controllers as well 
+  
+  #to make these methods also available in at the views, just say that they are helper methods
+  helper_method :current_user, :logged_in?
+  
+   def current_user
+     current_user ||= User.find(session[:user_id]) if session[:user_id]
+   end
+  
+   def logged_in?
+     #to make a boolean value just add two ! in front of the var
+     !!current_user
+   end
+
+   def require_user 
+      if !logged_in?
+         flash[:danger] = "Please log in first."
+         redirect_to "/login"
+      end
+   end
+   
+  
 end
